189a79bb1d63b736eb1811b7739bc6b32c75eec8
Joseph <joseph.ligier@gmail.com>
2020-04-22 17:18:11 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+from aws_cdk import core
+
+import json
+
+from environment.environment_stack import EnvironmentStack
+
+
+# Read global configuration file 
+with open('environment/environment_conf.json') as config_file: 
+    global_conf = json.load(config_file)
+
+
+app = core.App()
+stage = app.node.try_get_context("stage")
+
+if stage is None :
+    stage = "default"
+
+print("# Deploy stage [{}]".format(stage))
+
+
+common_tags = []
+common_tags.append( core.CfnTag( key="Project", value=global_conf["global"]["project"]))
+common_tags.append( core.CfnTag( key="Stage", value=stage))
+
+
+
+EnvironmentStack(app, "environment-{}".format(stage), tags=common_tags, name_extension=global_conf["global"]["extension"]+stage, stage=stage )
+
+app.synth()
---
+++ environment/environment_stack.py
@@ -0,0 +1,42 @@
+from aws_cdk import (
+    core,
+    aws_ec2 as _ec2
+)
+
+
+class EnvironmentStack(core.Stack):
+
+    def __init__(self, scope: core.Construct, id: str, name_extension: str, stage:str, tags:[], **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        resource_name = name_extension+"-vpc.main"
+
+        self.vpc = _ec2.Vpc(self, 
+            resource_name,
+            cidr="10.0.0.0/16",
+            max_azs=2,
+            # configuration will create 3 groups in 2 AZs = 6 subnets.
+            subnet_configuration=[
+                #_ec2.SubnetConfiguration(
+                #subnet_type=_ec2.SubnetType.PUBLIC,
+                #name="Public",
+                #cidr_mask=24
+            #),
+            #_ec2.SubnetConfiguration(
+            #    subnet_type=_ec2.SubnetType.PRIVATE,
+            #    name="Private",
+            #    cidr_mask=24
+            #), 
+            _ec2.SubnetConfiguration(
+                subnet_type=_ec2.SubnetType.ISOLATED,
+                name="Isolated",
+                cidr_mask=28
+            )
+            ],
+            # nat_gateway_provider=_ec2.NatProvider.gateway(),
+            #nat_gateways=2,
+            )
+            
+        core.CfnOutput(self, "Output", 
+            export_name="VPC-{}".format(stage),
+            value=self.vpc.vpc_id)
